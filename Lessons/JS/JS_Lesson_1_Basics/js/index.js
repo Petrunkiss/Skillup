46fd8c1a63e0920a239157a25b4f206ab5807a63
Petrunkiss <Petrunkiss@gmail.com>
2022-12-23 01:37:51 +0000

    add JS_HW_1

--- Lessons/JS/JS_Lesson_1_Basics/js/index.js
+++ Lessons/JS/JS_Lesson_1_Basics/js/index.js
@@ -37,3 +37,70 @@
 // const song1 = "don't worry\nbe\nhuggies" // не нужно экранировать символы
 // console.log(song);
 
+// --------------------------------------- Методы строк --------------------------------------------
+
+
+// let message = "I can't get no satisfaction and fun";
+// const res = message.split(' ').reverse().join(' ');
+// console.log(Array.isArray(res));
+// console.log(res);
+
+// //заменяет слово по значению
+// let findWord = message.replace('satisfaction', 'pizza');
+
+// console.log(findWord);
+
+// --------------------------------------- Conditions --------------------------------------------
+
+// let admin = ' ';
+
+// if (admin) {
+//     console.log(1);
+// }
+
+// let red = 'red'
+// let yellow = 'yellow'
+// let green = 'green'
+
+// let res = green
+
+// switch (res) {
+//     case 'red':
+//         console.log('red');
+//     break;
+//     case 'yellow':
+//         console.log('yellow');
+//     break;
+//     case 'green':
+//         console.log('green');
+//     break;
+// }
+
+// --------------------------------------- Loops --------------------------------------------
+
+
+// //выведение высокосного года
+// for (let i = 0; i < 2022; i+=1) {
+//     if (i % 4 == 0) {  
+//         console.log(i);     
+//     }
+    
+// }
+
+// continue - пропускает 1 итерацию цикл
+// break - останавливает цикл
+
+// --------------------------------------- Console --------------------------------------------
+// const sara = 'Sara'
+
+// console.log(
+// `%c${sara}`, `color:green; font-size: 40px`
+// );
+
+// --------------------------------------- Task 1 --------------------------------------------
+
+const hours = 10
+const minutes = 25
+
+minutes === 0 ? console.log(`Time: ${hours} ч`) : console.log(`Time: ${hours} ч ${minutes} м`)
+
